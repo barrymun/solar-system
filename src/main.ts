f099303d37382a7aca23ddfdb6dfd1bb5dac8970
Neil Murphy <barrymun@tcd.ie>
2024-03-27 18:10:57 +0000

    added (twinkling) stars in the background

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import "assets/css/style.scss";
 import "assets/css/solar-system.scss";
 
-console.log("HERE");
+import { totalStars } from "utils/consts";
+
+function createStars() {
+  const body = document.querySelector("body")!;
+  for (let i = 0; i < totalStars; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 10 + 5}s`;
+    body.appendChild(star);
+  }
+}
+
+(async () => {
+  createStars();
+})();
